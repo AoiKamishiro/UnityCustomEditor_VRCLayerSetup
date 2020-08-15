bd0705d50b76df440bc70252f97b499d23d13719
AoiKamishiro <syouho.mizumoto@gmail.com>
2020-08-15 19:20:35 +0900

    Initial commit

---
+++ Assets/00Kamishiro/VRCLayerSetup/Editor/AKVRCLayerSetup.cs
@@ -0,0 +1,92 @@
+﻿/*
+ *  Zlib License
+ *  
+ *  Copyright (c) 2020 AoiKamishiro/神城葵
+ *  
+ *  
+ *  This software is provided 'as-is', without any express or implied warranty.
+ *  In no event will the authors be held liable for any damages arising from the use of this software.
+ *  
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it
+ *  freely, subject to the following restrictions:
+ *  
+ *     1. The origin of this software must not be misrepresented; you must not
+ *           claim that you wrote the original software. If you use this software
+ *           in a product, an acknowledgment in the product documentation would be
+ *           appreciated but is not required.
+ *           
+ *     2. Altered source versions must be plainly marked as such, and must not be
+ *          misrepresented as being the original software.
+ *          
+ *     3. This notice may not be removed or altered from any source distribution. 
+ *     
+ */
+
+//LastUpdate:2020/08/15(JST) 
+
+using UnityEditor;
+using UnityEngine;
+public class VRCLayerSetup : EditorWindow
+{
+    [MenuItem("Tools/Kamishiro/VRCLayerSetup", priority = 150)]
+    private static void OnEnable()
+    {
+        VRCLayerSetup window = GetWindow<VRCLayerSetup>("VRCLayerSetup");
+        window.minSize = new Vector2(320, 360);
+        window.Show();
+    }
+
+    //GUI Component
+    private const string version = "VRCLayerSetup V1.0 by 神城葵";
+    private const string linktext = "操作説明等はこちら";
+    private const string link = "https://github.com/AoiKamishiro/UnityCustomEditor_VRCLayerSetup";
+
+    private void OnGUI()
+    {
+        //Editor Window
+        using (new GUILayout.VerticalScope())
+        {
+            EditorGUILayout.LabelField(version);
+            if (GUILayout.Button(linktext)) { OpenLink(link); }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Layer 設定", EditorStyles.boldLabel);
+            if (UpdateLayers.AreLayersSetup())
+            {
+                EditorGUILayout.LabelField("レイヤーは VRChat 向けに設定されています。");
+            }
+            else
+            {
+                if (GUILayout.Button("Setup Layers"))
+                {
+                    if (EditorUtility.DisplayDialog("Setup Layers for VRChat", "プロジェクトのレイヤー設定を VRChat 用に設定します。よろしいですか？", "OK", "Cancel"))
+                    {
+                        UpdateLayers.SetupEditorLayers();
+                    }
+                }
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Collision 設定", EditorStyles.boldLabel);
+            if (UpdateLayers.IsCollisionLayerMatrixSetup())
+            {
+                EditorGUILayout.LabelField("レイヤー毎衝突判定は VRChat 向けに設定されています。");
+            }
+            else
+            {
+                if (GUILayout.Button("Setup Collision Matrix"))
+                {
+                    if (EditorUtility.DisplayDialog("Setup Collision Matrix for VRChat", "プロジェクトのレイヤー毎衝突判定を VRChat 用に設定します。よろしいですか？", "OK", "Cancel"))
+                    {
+                        UpdateLayers.SetupCollisionLayerMatrix();
+                    }
+                }
+            }
+        }
+    }
+    private void OpenLink(string link)
+    {
+        Application.OpenURL(link);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# UnityCustomEditor_VRCLayerSetup
- 
+# VRCLayerSetup
+<H2><a href="https://github.com/AoiKamishiro/UnityCustomEditor_VRCLayerSetup/releases">最新版のダウンロードはこちら</a></H2>
+<H3>※更新の際は以前のバージョンを削除してからインポートしてください。</H3>
+<H3>※前提アセット</H3>
+<ul>
+<li>VRCSDK</li>
+</ul>
+<H3>※起動方法</H3>
+<a>Tppls/Kamishiro/VRCLayerSetup から起動できます。</a>
+<H3>プログラムについて</H3>
+<a>VRChat用のレイヤー設定をセットアプ出来ない、Avatar3.0用SDKでレイヤーをセットアップできます。</a>
+<H4>レイヤー設定をするメリット</H4>
+<a>カメラやライトなど、レイヤーに依存しているコンポーネントをアバターに仕込む時に、既存のプロジェクトなどからレイヤー設定を参照したりする必要がなくなります。</a>
+<H3>連絡先</H3>
+<a>Twitter : @aoi3192</a>
+<br/>
+<a>VRC : 神城 葵[Aoi_JPN]</a>
+
